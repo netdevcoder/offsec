a9289779da32321d71cce03d5916cfc22cc98974
netdevcoder <44331420+netdevcoder@users.noreply.github.com>
2018-11-23 16:55:51 +0700

    Update httpsocket.py

--- HTTP/httpsocket.py
+++ HTTP/httpsocket.py
@@ -1,15 +1,15 @@
+#!/usr/bin/python
+
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-if s.connect_ex("web server", 80):
+if s.connect_ex(("web_server", 80)):
    print("failed to connect")
-else:   
+else:
    s.sendall("GET / HTTP/1.1\r\n")
-   s.sendall("host: web server\r\n")
+   s.sendall("host: web_server\r\n\r\n")
    print s.recv(4096)
    s.close()
-   
-
 
 
